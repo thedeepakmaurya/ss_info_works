b821806bd2d09cbf8194ec07b63612a94339e1fa
thedeepakmaurya <deipakmaurya@gmail.com>
2025-05-21 00:12:44 +0530

    image changes

--- components/blocks/ServicesCard.jsx
+++ components/blocks/ServicesCard.jsx
@@ -1,6 +1,6 @@
 const ServicesCard = ({ title, highlights, description }) => {
   return (
-    <div className="rounded-xl p-2 shadow">
+    <div className="rounded-xl h-full transition-all hover:scale-105 p-2 shadow">
       <div
         className={`${title === "Custom Software Development" ? "bg-yellow-100" : title === "MLM Software Development" ? "bg-green-100" : title === "Web Development / Design" ? "bg-red-100" : title === "Android & iOS Development" ? "bg-blue-100" : title === "SEO/SMO & Marketing" ? "bg-cyan-100" : "bg-fuchsia-100"} h-full space-y-2 rounded-xl p-6 shadow`}
       >
